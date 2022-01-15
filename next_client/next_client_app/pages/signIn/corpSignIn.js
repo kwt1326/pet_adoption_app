21ec745c931bba30000deee3b78dca5b7096d6cc
worldi <jongha2788@u.sogang.ac.kr>
2022-01-15 10:54:18 +0900

    POST_LIST

--- next_client/next_client_app/pages/signIn/corpSignIn.js
+++ next_client/next_client_app/pages/signIn/corpSignIn.js
@@ -4,8 +4,12 @@ import style from "./signIn.module.scss";
 import { useMutation } from "@apollo/client";
 import { CORP_SIGN_UP_QUERY } from "../../quries/authQuery";
 
+import { useMutation } from "@apollo/client";
+import { SIGN_UP_QUERY2 } from "../../quries/authQuery";
 function corpSignIn() {
   const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
     nickname: "",
     companyName: "",
     address: "",
@@ -14,7 +18,15 @@ function corpSignIn() {
     email: "",
     password: "",
   });
-  const { name, password, nickname, companyName, address, phoneNumber, pageUri } = inputs;
+  const {
+    name,
+    password,
+    nickname,
+    companyName,
+    address,
+    phoneNumber,
+    pageUri,
+  } = inputs;
 
   const onChange = (e) => {
     const { value, name } = e.target;
@@ -40,7 +52,6 @@ function corpSignIn() {
       },
     });
   };
-
   return (
     <div>
       <Header children={"업체 회원가입"} />
@@ -48,25 +59,74 @@ function corpSignIn() {
         <div className={style.infoContainer}>
           <h4>개인정보</h4>
           <div>
-            <input name="email" onChange={onChange} value={name} className={style.inputArea} placeholder="이메일을 입력하세요"></input>
+            <input
+              className={style.inputArea}
+              placeholder="이메일을 입력하세요"
+              onChange={onChange}
+              value={email}
+              name="email"
+            ></input>
             <button className={style.button}>조회</button>
           </div>
-          <input name="password" onChange={onChange} value={password} className={style.inputArea} type="password" placeholder="비밀번호를 입력하세요"></input>
-          <input className={style.inputArea} type="password" placeholder="비밀번호를 재입력하세요"></input>
+          <input
+            className={style.inputArea}
+            type="password"
+            value={password}
+            onChange={onChange}
+            name="password"
+            placeholder="비밀번호를 입력하세요"
+          ></input>
+          <input
+            className={style.inputArea}
+            type="password"
+            onChange={onChange}
+            name="password"
+            placeholder="비밀번호를 재입력하세요"
+          ></input>
           <div>
-            <input name="nickname" onChange={onChange} value={nickname} className={style.inputArea} placeholder="닉네임을 입력하세요"></input>
+            <input
+              className={style.inputArea}
+              value={nickname}
+              onChange={onChange}
+              name="nickname"
+              placeholder="닉네임을 입력하세요"
+            ></input>
             <button className={style.button}>중복확인</button>
           </div>
         </div>
         <div className={style.infoContainer}>
           <h4>기업정보</h4>
-          <input name="companyName" onChange={onChange} value={companyName} className={style.inputArea} placeholder="업체명을 입력하세요"></input>
+          <input
+            className={style.inputArea}
+            placeholder="업체명을 입력하세요"
+            onChange={onChange}
+            value={companyName}
+            name="companyName"
+          ></input>
           <div>
-            <input name="address" onChange={onChange} value={address} className={style.inputArea} placeholder="주소를 입력하세요"></input>
+            <input
+              className={style.inputArea}
+              placeholder="주소를 입력하세요"
+              onChange={onChange}
+              value={address}
+              name="address"
+            ></input>
             <button className={style.button}>주소찾기</button>
           </div>
-          <input name="phoneNumber" onChange={onChange} value={phoneNumber} className={style.inputArea} placeholder="전화번호를 입력하세요"></input>
-          <input name="pageUri" onChange={onChange} value={pageUri} className={style.inputArea} placeholder="홈페이지를 입력하세요"></input>
+          <input
+            className={style.inputArea}
+            placeholder="전화번호를 입력하세요"
+            onChange={onChange}
+            value={phoneNumber}
+            name="phoneNumber"
+          ></input>
+          <input
+            className={style.inputArea}
+            placeholder="홈페이지를 입력하세요"
+            onChange={onChange}
+            value={pageUri}
+            name="pageUri"
+          ></input>
         </div>
         <div className={[style.infoContainer, style.picture].join(" ")}>
           <h4>사진정보</h4>
--- next_client/next_client_app/quries/adoptionPostQuery.ts
+++ next_client/next_client_app/quries/adoptionPostQuery.ts
@@ -8,3 +8,55 @@ export const GET_ADOPTION_POST_LIST = gql`
     }
   }
 `;
+
+export const GET_ADOPTION_POST_LIST2 = gql`
+query getPosts($input: GetAdoptionPostArgs!) {
+  getPosts(getPostsArgs: $input) {
+    title
+    content
+    writter {
+      nickname
+      companyName
+      address
+      phoneNumber
+      pageUri
+      isAuthenticated
+      isProfit
+      authenticatedAt
+    }
+    pet {
+      registrant {
+        nickname
+        companyName
+        address
+        phoneNumber
+        pageUri
+        isAuthenticated
+        isProfit
+        authenticatedAt
+      }
+      name
+      breed
+      type
+      price
+      age
+      weight
+      isGenderMale
+      vaccinated
+      neutered
+      characteristic
+      othersInfo
+    }
+  }
+}`;
+
+
+
+export const CREATE_POST_MUTATION = gql`
+mutation createAdoptionPost($input: CreateAdoptionPostArgs!) {
+  createAdoptionPost(postArgs: $input) {
+    result
+    id
+ }
+}
+`;
