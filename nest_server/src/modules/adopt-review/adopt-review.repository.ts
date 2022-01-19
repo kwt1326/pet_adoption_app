4569c8db8dfa20c0e8076529f515eb875c958c27
hminn <hmkim0008@gmail.com>
2022-01-19 18:11:17 +0900

    feat: 입양 리뷰 단일 조회 API 구현

--- nest_server/src/modules/adopt-review/adopt-review.repository.ts
+++ nest_server/src/modules/adopt-review/adopt-review.repository.ts
@@ -1,7 +1,20 @@
+import { BadRequestException } from "@nestjs/common";
 import { AdoptReview } from "src/entities/adopt-review.entity";
 import { EntityRepository, Repository } from "typeorm";
 
 @EntityRepository(AdoptReview)
 export class AdoptReviewRepository extends Repository<AdoptReview>{
+  async getOneAdoptReviewById(id: number) {
+    const review = await this
+      .createQueryBuilder('review')
+      .leftJoinAndSelect('review.adopteeUser', 'adopteeUser')
+      .leftJoinAndSelect('adopteeUser.user', 'user')
+      .where('review.id = :id', { id })
+      .getOne();
 
+    if (!review) {
+      throw new BadRequestException(`There is no review with id of ${id}`);
+    }
+    return review;
+  }
 }
