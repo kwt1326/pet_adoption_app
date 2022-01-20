846482e5de835c7fdaaa6848ef7301ea0c30d949
hminn <hmkim0008@gmail.com>
2022-01-20 22:38:50 +0900

    fix: 입양 리뷰 좋아요 엔터티 post 필드 수정
    - 입양 리뷰 게시물에 대한 좋아요 데이터이므로 likePost는 AdoptReview.

--- nest_server/src/entities/adopt-review-like.entity.ts
+++ nest_server/src/entities/adopt-review-like.entity.ts
@@ -1,7 +1,7 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { AdoptReview } from './adopt-review.entity';
 import { AdopteeUser } from './adoptee-user.entity';
-import { AdoptionPost } from './adoption-post.entity';
 import { CoreIdEntity } from './common/core.entity';
 
 // '좋아요(하트)' 테이블 - 입양자만 사용
@@ -15,8 +15,8 @@ export class AdoptionReviewLike extends CoreIdEntity {
   @Field(() => AdopteeUser)
   adopteeUser: AdopteeUser;
 
-  @ManyToOne(() => AdoptionPost, { onDelete: 'CASCADE'})
+  @ManyToOne(() => AdoptReview, { onDelete: 'CASCADE'})
   @JoinColumn()
-  @Field(() => AdoptionPost)
-  likePost: AdoptionPost;
+  @Field(() => AdoptReview)
+  likePost: AdoptReview;
 }
--- nest_server/src/modules/adopt-review/adopt-review.module.ts
+++ nest_server/src/modules/adopt-review/adopt-review.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdoptionReviewLike } from 'src/entities/adopt-review-like.entity';
 import { AdoptReview } from '../../entities/adopt-review.entity';
 import { AdopteeUser } from '../../entities/adoptee-user.entity';
 import { AdopteeUserRepository } from '../user/user.repository';
@@ -12,6 +13,7 @@ import { AdoptReviewService } from './adopt-review.service';
     TypeOrmModule.forFeature([
       AdopteeUser,
       AdoptReview,
+      AdoptionReviewLike,
       AdoptReviewRepository,
       AdopteeUserRepository,
       AdoptReviewPictureRepository,
