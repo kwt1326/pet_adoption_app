1e6ac4b70e63a7cc1b289c6387fd8a029eef4356
yangBasak <yangBasak@gmail.com>
2022-01-29 14:04:25 +0900

    로그인 작업중

--- next_client/next_client_app/pages/login/index.js
+++ next_client/next_client_app/pages/login/index.js
@@ -8,37 +8,57 @@ import { useLazyQuery } from "@apollo/client";
 import { LOGIN_QUERY } from "../../quries/authQuery";
 
 function login() {
-  const [inputs, setInputs] = useState({
-    email: "",
-    password: "",
+  // const [inputs, setInputs] = useState({
+  //   email: "",
+  //   password: "",
+  // });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [trigger, setTrigger] = useState({
+    isComplete: true,
   });
-  const { email, password } = inputs;
+  // const { email, password } = inputs;
 
   const onChange = (e) => {
     const { value, name } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+    // setInputs({
+    //   ...inputs,
+    //   [name]: value,
+    // });
   };
-
   const [loginQuery, { data: token }] = useLazyQuery(LOGIN_QUERY);
+  // const [loginQuery] = useLazyQuery(LOGIN_QUERY);
 
   // GraphQL / LOGIN QUERY
   const loginFunc = async () => {
-    const response = await loginQuery({
-      variables: {
-        input: {
-          email: inputs.email,
-          password: inputs.password,
+    if (email === "" || password === "") {
+      setTrigger(() => {
+        return { isComplete: false };
+      });
+    } else {
+      setTrigger(() => {
+        return { isComplete: true };
+      });
+      const response = await loginQuery({
+        variables: {
+          input: {
+            email: email,
+            password: password,
+          },
         },
-      },
-    });
-    console.log("RESPONSE", response, token);
-    const responseData = response?.data?.login;
-    if (responseData) {
-      console.log(responseData.result.token);
-      cookie.set(process.env.JWT_KEY, responseData.result.token);
+      });
+      console.log(response);
+      console.log(token);
+      const responseData = response?.data?.login;
+      if (responseData) {
+        console.log(responseData.result.token);
+        cookie.set(process.env.JWT_KEY, responseData.result.token);
+      }
     }
   };
 
@@ -47,31 +67,21 @@ function login() {
       <Header children={"로그인"} />
       <div className={style.container}>
         <div className={style.inputArea}>
-          <input
-            name="email"
-            onChange={onChange}
-            value={email}
-            placeholder="email을 입력하세요"
-          ></input>
-          <input
-            name="password"
-            onChange={onChange}
-            value={password}
-            type="password"
-            placeholder="비밀번호를 입력하세요"
-          ></input>
+          <input name="email" onChange={onChange} value={email} placeholder="email을 입력하세요"></input>
+          <input name="password" onChange={onChange} value={password} type="password" placeholder="비밀번호를 입력하세요"></input>
           <button onClick={loginFunc}>로그인</button>
         </div>
+        {trigger.isComplete === false ? "아이디, 비밀번호를 모두 입력하세요" : null}
         <div className={style.search}>
           <div>아이디 찾기</div>
           <div>비밀번호 찾기</div>
         </div>
         <div className={style.join}>
-          <Link href="../signIn/personalSignIn">
+          <Link href={`signIn/personalSignIn`}>
             <a>개인 회원가입</a>
           </Link>
           <span> / </span>
-          <Link href="../signIn/corpSignIn">
+          <Link href="signIn/corpSignIn">
             <a>업체 회원가입</a>
           </Link>
         </div>
--- next_client/next_client_app/pages/signIn/corpSignIn.js
+++ next_client/next_client_app/pages/signIn/corpSignIn.js
@@ -4,8 +4,8 @@ import style from "./signIn.module.scss";
 import { useMutation } from "@apollo/client";
 import { CORP_SIGN_UP_QUERY } from "../../quries/authQuery";
 
-import { useMutation } from "@apollo/client";
-import { SIGN_UP_QUERY2 } from "../../quries/authQuery";
+// import { useMutation } from "@apollo/client";
+// import { SIGN_UP_QUERY2 } from "../../quries/authQuery";
 function corpSignIn() {
   const [inputs, setInputs] = useState({
     email: "",
@@ -18,15 +18,7 @@ function corpSignIn() {
     email: "",
     password: "",
   });
-  const {
-    name,
-    password,
-    nickname,
-    companyName,
-    address,
-    phoneNumber,
-    pageUri,
-  } = inputs;
+  const { name, password, nickname, companyName, address, phoneNumber, pageUri } = inputs;
 
   const onChange = (e) => {
     const { value, name } = e.target;
@@ -59,74 +51,25 @@ function corpSignIn() {
         <div className={style.infoContainer}>
           <h4>개인정보</h4>
           <div>
-            <input
-              className={style.inputArea}
-              placeholder="이메일을 입력하세요"
-              onChange={onChange}
-              value={email}
-              name="email"
-            ></input>
+            <input className={style.inputArea} placeholder="이메일을 입력하세요" onChange={onChange} value={email} name="email"></input>
             <button className={style.button}>조회</button>
           </div>
-          <input
-            className={style.inputArea}
-            type="password"
-            value={password}
-            onChange={onChange}
-            name="password"
-            placeholder="비밀번호를 입력하세요"
-          ></input>
-          <input
-            className={style.inputArea}
-            type="password"
-            onChange={onChange}
-            name="password"
-            placeholder="비밀번호를 재입력하세요"
-          ></input>
+          <input className={style.inputArea} type="password" value={password} onChange={onChange} name="password" placeholder="비밀번호를 입력하세요"></input>
+          <input className={style.inputArea} type="password" onChange={onChange} name="password" placeholder="비밀번호를 재입력하세요"></input>
           <div>
-            <input
-              className={style.inputArea}
-              value={nickname}
-              onChange={onChange}
-              name="nickname"
-              placeholder="닉네임을 입력하세요"
-            ></input>
+            <input className={style.inputArea} value={nickname} onChange={onChange} name="nickname" placeholder="닉네임을 입력하세요"></input>
             <button className={style.button}>중복확인</button>
           </div>
         </div>
         <div className={style.infoContainer}>
           <h4>기업정보</h4>
-          <input
-            className={style.inputArea}
-            placeholder="업체명을 입력하세요"
-            onChange={onChange}
-            value={companyName}
-            name="companyName"
-          ></input>
+          <input className={style.inputArea} placeholder="업체명을 입력하세요" onChange={onChange} value={companyName} name="companyName"></input>
           <div>
-            <input
-              className={style.inputArea}
-              placeholder="주소를 입력하세요"
-              onChange={onChange}
-              value={address}
-              name="address"
-            ></input>
+            <input className={style.inputArea} placeholder="주소를 입력하세요" onChange={onChange} value={address} name="address"></input>
             <button className={style.button}>주소찾기</button>
           </div>
-          <input
-            className={style.inputArea}
-            placeholder="전화번호를 입력하세요"
-            onChange={onChange}
-            value={phoneNumber}
-            name="phoneNumber"
-          ></input>
-          <input
-            className={style.inputArea}
-            placeholder="홈페이지를 입력하세요"
-            onChange={onChange}
-            value={pageUri}
-            name="pageUri"
-          ></input>
+          <input className={style.inputArea} placeholder="전화번호를 입력하세요" onChange={onChange} value={phoneNumber} name="phoneNumber"></input>
+          <input className={style.inputArea} placeholder="홈페이지를 입력하세요" onChange={onChange} value={pageUri} name="pageUri"></input>
         </div>
         <div className={[style.infoContainer, style.picture].join(" ")}>
           <h4>사진정보</h4>
