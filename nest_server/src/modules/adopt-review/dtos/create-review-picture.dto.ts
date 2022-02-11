6ab26ead64ca53d9d06be46b7d9e80da718ba100
hminn <hmkim0008@gmail.com>
2022-02-11 18:28:53 +0900

    style: 이전 코드 lint 적용

--- nest_server/src/modules/adopt-review/dtos/create-review-picture.dto.ts
+++ nest_server/src/modules/adopt-review/dtos/create-review-picture.dto.ts
@@ -1,9 +1,9 @@
-import { Field, InputType } from "@nestjs/graphql";
+import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class CreateAdoptReviewPictureInput {
   @Field(() => Number, {
-    description: '사진을 업로드 할 리뷰 게시물의 id'
+    description: '사진을 업로드 할 리뷰 게시물의 id',
   })
   reviewId: number;
 
