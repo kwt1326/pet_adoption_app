14d296081669d51cb1ac3297f898857211444c41
yangBasak <yangBasak@gmail.com>
2022-02-18 21:28:33 +0900

    작업중

--- next_client/next_client_app/pages/myPage/index.js
+++ next_client/next_client_app/pages/myPage/index.js
@@ -1,14 +1,46 @@
 import React, { useState } from "react";
 import { useUserInfo } from "../../hooks/user";
+import style from "./mypage.module.scss";
+import Header from "../../components/Header/index";
+import { FaFile, FaBoxOpen, FaAngleRight } from "react-icons/fa";
 
 function myPage() {
+  let email = '';
   const userInfo = useUserInfo();
-  console.log(userInfo);
+  // while (true) {
+  //   try {
+  // email = userInfo.email;
+  // console.log(email)
+  //     break;
+  //   } catch (err) {
+  //     console.log(err)
+
+  //   }
+  // }
+
+  // 
+  // console.log(email)
   return (
     <div>
-      <span></span>
-      <span>..</span>
-    </div>
+      <Header children={"마이페이지"} />
+      <div>
+        <div>
+          <div>
+            <span>{ }</span>님, 안녕하세요!
+          </div>
+          <span>{ }</span>
+        </div>
+        <ul>
+          <li style={{ display: "flex", justifyContent: "space-between" }} >
+            <div>
+              <FaFile />
+              <span>개인정보 수정</span>
+            </div>
+            <FaAngleRight />
+          </li>
+        </ul>
+      </div>
+    </div >
   );
 }
 
