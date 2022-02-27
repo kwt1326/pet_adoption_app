a136b6c400075823b64e8df1324fe1a1f6534446
kwt1326 <u1326@hotmail.com>
2022-02-28 00:30:13 +0900

    fix: unresolved path

--- next_client/next_client_app/components/Header/index.tsx
+++ next_client/next_client_app/components/Header/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { FaHome, FaChevronLeft } from "react-icons/fa";
 
-import Sidebar from "../Modal/sidebar";
+import Sidebar from "../SideBar";
 import styles from "./header.module.scss";
 import { useRouter } from "next/router";
 
