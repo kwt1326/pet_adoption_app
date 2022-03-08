7822065dfbc624d8d6877518fc54f9d085359c98
kwt1326 <u1326@hotmail.com>
2022-03-08 11:19:01 +0900

    fix: permission

---
+++ next_client/react-native/pages/Webview.tsx
@@ -0,0 +1,50 @@
+import React, { createRef, useCallback } from 'react';
+import { Alert, NativeSyntheticEvent, Platform } from 'react-native';
+import { FileDownload, WebView } from 'react-native-webview';
+import { WebViewMessage } from 'react-native-webview/lib/WebViewTypes';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
+import { webViewUri } from '../config';
+
+function MainWebView() {
+  let webViewRef = createRef<WebView>();
+  const { getItem, setItem, removeItem } = useAsyncStorage('cookie');
+
+  const saveToken = useCallback(
+    async (data: string) => {
+      setItem(data);
+    },
+    [setItem],
+  );
+
+  const onFileDownload = ({ nativeEvent }: { nativeEvent: FileDownload }) => {
+    Alert.alert('File download detected', nativeEvent.downloadUrl); // TODO: REMOVE
+  };
+
+  const _onMessage = async (event: NativeSyntheticEvent<WebViewMessage>) => {
+    console.log(event.nativeEvent);
+    const { data } = event.nativeEvent;
+    await saveToken(data);
+    const items = await getItem();
+    if (items) {
+      console.log('TEST', JSON.parse(items));
+    }
+  };
+
+  const platformProps = Platform.select({
+    ios: {
+      onFileDownload: onFileDownload,
+    },
+  });
+
+  return (
+    <WebView
+      ref={webViewRef}
+      source={{ uri: webViewUri }}
+      automaticallyAdjustContentInsets={true}
+      onMessage={_onMessage}
+      {...platformProps}
+    />
+  );
+}
+
+export default MainWebView;
