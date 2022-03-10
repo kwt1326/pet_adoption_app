abfb9c1af8af0c7ab73f4afa821e566fe58a1691
yangBasak <yangBasak@gmail.com>
2022-03-10 13:20:58 +0900

    전화번호 수정

--- next_client/next_client_app/components/SignInput/index.js
+++ next_client/next_client_app/components/SignInput/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import style from "./SingInput.module.scss";
 
-function SignInput({ value, onChange, placeholder, type, errorText }) {
+function SignInput({ value, onChange, placeholder, type, errorText, maxLength, onkeypress }) {
   return (
     <div className={style.container}>
-      <input value={value} onChange={onChange} placeholder={placeholder} type={type} className={style.inputContent}></input>
+      <input value={value} onChange={onChange} placeholder={placeholder} type={type} maxLength={maxLength} onKeyPress={onkeypress} className={style.inputContent} ></input>
       {errorText !== "" ? <div style={{ marginTop: "5px", fontSize: "12px", color: "crimson" }}>{errorText}</div> : null}
     </div>
   );
