b15f38f8cb45afd7b22ee913af84c47fd53de145
kwt1326 <u1326@hotmail.com>
2022-03-16 09:16:35 +0900

    fix: login page bug

--- next_client/next_client_app/hooks/user/index.tsx
+++ next_client/next_client_app/hooks/user/index.tsx
@@ -1,6 +1,9 @@
-import { GetServerSidePropsContext } from 'next';
 import Cookie from 'universal-cookie';
 import JsonWebToken from 'jsonwebtoken';
+import { useQuery } from "@apollo/client";
+import { GetServerSidePropsContext } from 'next';
+
+import { GET_ONE_ADOPTEE_USER, GET_ONE_ADOPT_USER } from "../../quries/userFindQuery";
 
 export function useUserInfo(ctx?: GetServerSidePropsContext) {
   let cookie = null;
@@ -12,4 +15,25 @@ export function useUserInfo(ctx?: GetServerSidePropsContext) {
   const token = cookie.get(process.env.JWT_KEY)
   const decode = token ? JsonWebToken.decode(token) : undefined
   return decode
+}
+
+export function useUserDetail() {
+  const userInfo = useUserInfo();
+  let oneUser = {};
+  if (userInfo?.userType === "ADOPTEE_USER") {
+    const { data } = useQuery(GET_ONE_ADOPTEE_USER, {
+      variables: {
+        id: userInfo?.id,
+      },
+    });
+    oneUser = data?.getOneAdopteeUser;
+  } else if (userInfo?.userType === "ADOPT_USER") {
+    const { data } = useQuery(GET_ONE_ADOPT_USER, {
+      variables: {
+        id: userInfo?.id,
+      },
+    });
+    oneUser = data?.getOneAdoptUser;
+  }
+  return oneUser;
 }
